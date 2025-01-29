6d6644ed36bff540ff53aff622b85caaab6f3b32
kouki0404 <168718919+kouki0404@users.noreply.github.com>
2025-01-29 08:45:22 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,11 +9,53 @@ if 'c' not in st.session_state:
     st.session_state.c = random.randint(1,9)
 if 'd' not in st.session_state:
     st.session_state.d = random.randint(1,9)
-col1,col2,col3,col4,col5,col6 = st.columns(6)
+col1,col2,col3,col4,col5,col6,col7,col8 = st.columns(8)
 sisoku = ["+","-","×","÷","(",")"]
 answer = []
-col1.button(str(st.session_state.a))
-col2.button(str(st.session_state.b))
-col3.button(str(st.session_state.c))
-col4.button(str(st.session_state.d))
+if st.session_state.a == st.session_state.b:
+    col1.button(f"{str(st.session_state.a)}")
+    col2.button(f"{str(st.session_state.b)} ")
+    if st.session_state.a == st.session_state.c:
+        col3.button(f"{str(st.session_state.c)}  ")
+        if st.session_state.a == st.session_state.d:
+            col4.button(f"{str(st.session_state.d)}   ")
+        else:
+            col4.button(f"{str(st.session_state.d)}")
+    else:
+        if st.session_state.c == st.session_state.d:
+            col3.button(f"{str(st.session_state.c)} ")
+            col4.button(f"{str(st.session_state.d)}")
+        else:
+            col3.button(f"{str(st.session_state.c)}")
+            col4.button(f"{str(st.session_state.d)}")
+elif st.session_state.a == st.session_state.c :
+    col1.button(f"{str(st.session_state.a)}")
+    col2.button(f"{str(st.session_state.b)} ")
+    col3.button(f"{str(st.session_state.c)} ")
+    col4.button(f"{str(st.session_state.d)}")
+elif st.session_state.a == st.session_state.d:
+    col1.button(f"{str(st.session_state.a)}")
+    col2.button(f"{str(st.session_state.b)}")
+    col3.button(f"{str(st.session_state.c)} ")
+    col4.button(f"{str(st.session_state.d)} ")
+elif st.session_state.b == st.session_state.c and st.session_state.b != st.session_state.d:
+    col1.button(f"{str(st.session_state.a)}")
+    col2.button(f"{str(st.session_state.b)}")
+    col3.button(f"{str(st.session_state.c)} ")
+    col4.button(f"{str(st.session_state.d)}")
+elif st.session_state.b == st.session_state.d:
+    col1.button(f"{str(st.session_state.a)}")
+    col2.button(f"{str(st.session_state.b)} ")
+    col3.button(f"{str(st.session_state.c)}")
+    col4.button(f"{str(st.session_state.d)}")
+elif st.session_state.c == st.session_state.d:
+    col1.button(f"{str(st.session_state.a)}")
+    col2.button(f"{str(st.session_state.b)}")
+    col3.button(f"{str(st.session_state.c)} ")
+    col4.button(f"{str(st.session_state.d)}")
+else:
+    col1.button(f"{str(st.session_state.a)}")
+    col2.button(f"{str(st.session_state.b)}")
+    col3.button(f"{str(st.session_state.c)}")
+    col4.button(f"{str(st.session_state.d)}")
 col1.button(str(sisoku[0]))
